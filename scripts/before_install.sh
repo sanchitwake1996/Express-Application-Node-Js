fb8856319b810391faa81673899215fb6bcd31d1
sanchitwake1996 <sanchitwake1997@gmail.com>
2025-01-31 18:41:10 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Stopping old application..."
-sudo systemctl stop myapp || true
-echo "Cleaning up old files..."
-rm -rf /home/ubuntu/app/*
+sudo apt update -y
+sudo apt install -y nodejs npm
+
