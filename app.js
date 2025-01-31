06d5f36a1c4eb944090038099fad8d236a075cc3
sanchitwake1996 <sanchitwake1997@gmail.com>
2025-01-31 18:39:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World! Deployed using CodeDeploy on Ubuntu!");
+app.get('/', (req, res) => {
+    res.send('Hello from CodePipeline deployment!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
